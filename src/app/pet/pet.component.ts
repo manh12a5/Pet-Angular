db8e660118b35f9c4c0a245a7daf28568b8dda7e
manh12a5 <73921743+manh12a5@users.noreply.github.com>
2021-04-06 15:31:09 +0700

    Add BootStrap

--- src/app/pet/pet.component.ts
+++ src/app/pet/pet.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-pet',
   templateUrl: './pet.component.html',
-  styleUrls: ['./pet.component.scss']
+  styleUrls: ['./pet.component.css']
 })
 export class PetComponent implements OnInit {
   petName = 'puppie';
